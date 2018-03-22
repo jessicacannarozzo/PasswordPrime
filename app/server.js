bdaf150b18ca011d094ce1662223d7ea18fab9e7
Jess <jesscannarozzo@cmail.carleton.ca>
2018-03-22 15:53:37 -0400

    quote API working

--- app/server.js
+++ app/server.js
@@ -1,10 +1,45 @@
 // Backend @authors: Jess Cannarozzo & Karla Martins-Spuldaro
-
 const express = require('express');
 const app = express();
+const request = require('request');
 
-app.get('/', (req,res) => {
-    res.send(200);
+//routes
+app.get('/', (req,res) => { 
+    res.sendStatus(200);
 });
+
+app.get('/quotes', (req,res) => { 
+    request.get({
+        url: "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?",
+    }, (error, message,body) => {
+        if (!error && message.statusCode === 200) {
+            if (body.charAt(0) === '?') { //format
+                body = body.substring(2,body.length-1);
+            }
+            
+            // createQuotePW(body);
+            obj = {
+                pw: createQuotePW(body),
+                text: JSON.parse(body)
+            };
+            res.send(obj);
+        }
+    });
+});
+
+app.get('/colours', (req,res) => { 
+    res.sendStatus(200);
+});
+
+app.get('/credits', (req,res) => { 
+    res.sendStatus(200);
+});
+
+//methods
+function createQuotePW(quote) {
+    var quotePW = quote.match(/\b(\w)/g).join('');
+    console.log(quotePW);
+    return quotePW;
+}
 
 app.listen(3000, () => console.log("We're live on 3000! In style."));

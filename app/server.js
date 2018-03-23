0220148ac56826fa008694ce5e775b69cc8c9c57
Jess <jesscannarozzo@cmail.carleton.ca>
2018-03-22 20:33:01 -0400

    moved quote creation into getQuote()

--- app/server.js
+++ app/server.js
@@ -4,31 +4,18 @@ const app = express();
 const request = require('request');
 const quotesURL = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
 
+const quoteLen = 60;
+
 //routes
-app.get('/', (req,res) => { 
+app.get('/', (req, res) => {
     res.sendStatus(200);
 });
 
 // output: passwordObj with generated quote password, quote, author, quote link
-app.get('/quotes', (req,res) => { 
-    request.get({
-        url: quotesURL
-    }, (error, message,body) => {
-        if (!error && message.statusCode === 200) {
-            if (body.charAt(0) === '?') { //format
-                body = body.substring(2,body.length-1);
-            }
-            
-            // createQuotePW(body);
-            passwordObj = {
-                pw: createQuotePW(JSON.parse(body).quoteText),
-                quote: JSON.parse(body).quoteText,
-                author: JSON.parse(body).quoteAuthor,
-                link: JSON.parse(body).quoteLink
-            };
-            res.send(passwordObj);
-        }
-    });
+app.get('/quotes', (req, res) => { 
+    // var result = getQuote(handleQuote);
+
+    getQuote(res);
 });
 
 app.get('/colours', (req,res) => { 
@@ -40,13 +27,41 @@ app.get('/credits', (req,res) => {
 });
 
 //methods
-
 //input: quote (string)
 //output: create password from first letter of each word in the quote
 function createQuotePW(quote) {
-    var quotePW = quote.match(/\b(\w)/g).join('');
+    const quotePW = quote.match(/\b(\w)/g).join('');
     console.log(quotePW);
     return quotePW;
+}
+
+function getQuote(cb) {
+
+    request.get({
+        url: quotesURL
+    }, (error, message,body) => {
+        if (!error && message.statusCode === 200) {          
+            if (body.charAt(0) === '?') { //format
+                body = body.substring(2,body.length-1);
+            }
+
+            let quote = body && JSON.parse(body);
+
+            // console.log(JSON.parse(body).quoteText.length);
+            
+            
+            const passwordObj = {
+                pw: createQuotePW(quote.quoteText),
+                quote: quote.quoteText,
+                author: quote.quoteAuthor,
+                link: quote.quoteLink
+            };
+            return cb.send(passwordObj)
+        }
+    });
+}
+
+function formatQuote() {
 }
 
 app.listen(3000, () => console.log("We're live on 3000! In style."));

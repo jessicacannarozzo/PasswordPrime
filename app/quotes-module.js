cc4508585be351e5972bc3ac00219e68bab69d88
Jess <jesscannarozzo@cmail.carleton.ca>
2018-03-24 21:46:55 -0400

    fixed apostrophe bug

--- app/quotes-module.js
+++ app/quotes-module.js
@@ -1,14 +1,20 @@
 const request = require('request');
 const quotesURL = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
-const quoteLen = 60;
+const quoteLen = 40;
+const pwLen = 60;
+const symbols = "!@#$%^&";
+const numbers = "1234567890";
 
 //methods
 //input: quote (string)
 //output: create password from first letter of each word in the quote
 function createQuotePW(quote) {
     if (quote) {
-        const quotePW = quote.match(/\b(\w)/g).join('');
-        
+        quotePW = quote.match(/\b(\w)/g).join(''); //get first letter of each word in quote
+        // while (quotePw.length < pwLen) {
+        //     i = 
+        //     quotePw = quotePW.slice(0, )
+        // }
         return quotePW;
     }
 }
@@ -21,26 +27,24 @@ exports.getQuote = (cb) => {
         url: quotesURL
     }, (error, message,body) => {
         if (body) body = formatQuote(body);
-        if (!error && message.statusCode === 200 && JSON.parse(body).quoteText.length < quoteLen) {          
-        //    body = formatQuote(body);
+        if (!error && message.statusCode === 200 && (body && JSON.parse(body)).quoteText.length < quoteLen) {          
             let quote = {};
             
             try {
                 quote = JSON.parse(body);
+                const passwordObj = {
+                    pw: createQuotePW(quote.quoteText),
+                    quote: quote.quoteText,
+                    author: quote.quoteAuthor,
+                    link: quote.quoteLink
+                };
+                return cb.send(passwordObj)
             } catch(err) {
                 console.log(err);
             }
 
-            console.log(quote.quoteText);
-                        
-            
-            const passwordObj = {
-                pw: createQuotePW(quote.quoteText),
-                quote: quote.quoteText,
-                author: quote.quoteAuthor,
-                link: quote.quoteLink
-            };
-            return cb.send(passwordObj)
+            // console.log(quote.quoteText);
+
         } else {
             this.getQuote(cb); //find a new quote
         }
@@ -50,7 +54,14 @@ exports.getQuote = (cb) => {
 // removes odd characters from quote
 function formatQuote(body) {
     if (body.charAt(0) === '?') { //format
-        // console.log(body);
+        for (i = 0; i < body.length; i++) {
+            if (body.charAt(i) === '\'') {
+                console.log(body);
+                body = body.substring(0,i).concat("\\'").concat(body.substring(i+1,body.length));
+                console.log(body);             
+                i +=2 ;   
+            }
+        }
         return body.substring(2,body.length-1);
     }
 }

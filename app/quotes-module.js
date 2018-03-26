f3d89b5f3e1aecb85eacc5aed5efc1fb7fdd3ba8
Jess <jesscannarozzo@cmail.carleton.ca>
2018-03-26 16:24:03 -0400

    working towards adding special characters and numbers to password

--- app/quotes-module.js
+++ app/quotes-module.js
@@ -1,7 +1,7 @@
 const request = require('request');
 const quotesURL = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
-const quoteLen = 40;
-const pwLen = 60;
+const quoteLen = 50;
+const pwLen = 14;
 const symbols = "!@#$%^&";
 const numbers = "1234567890";
 
@@ -9,13 +9,19 @@ const numbers = "1234567890";
 //input: quote (string)
 //output: create password from first letter of each word in the quote
 function createQuotePW(quote) {
+    quotePw = "";
     if (quote) {
-        quotePW = quote.match(/\b(\w)/g).join(''); //get first letter of each word in quote
-        // while (quotePw.length < pwLen) {
-        //     i = 
-        //     quotePw = quotePW.slice(0, )
-        // }
-        return quotePW;
+        let quotePw = quote.match(/\b(\w)/g).join(''); //get first letter of each word in quote
+
+        function addSpecialCharacters(pw) {
+            while (pw.length < pwLen) {
+                console.log(pw);
+                pw += symbols[0];
+            }
+            return pw;
+        }
+        quotePw = addSpecialCharacters(quotePw);
+        return quotePw;
     }
 }
 
@@ -26,7 +32,7 @@ exports.getQuote = (cb) => {
     request.get({
         url: quotesURL
     }, (error, message,body) => {
-        if (body) body = formatQuote(body);
+        if (body) body = formatObj(body);
         if (!error && message.statusCode === 200 && (body && JSON.parse(body)).quoteText.length < quoteLen) {          
             let quote = {};
             
@@ -51,17 +57,31 @@ exports.getQuote = (cb) => {
     });
 }
 
-// removes odd characters from quote
-function formatQuote(body) {
+//removes escape character (\) in quotes with apostrophes
+//input: quote obj
+//output: quote (string)
+exports.formatQuote = (obj) => {
+    try {
+        let quote = obj.quoteText;
+        
+        return quote;
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+//removes odd characters from quote
+function formatObj(body) {
     if (body.charAt(0) === '?') { //format
         for (i = 0; i < body.length; i++) {
             if (body.charAt(i) === '\'') {
-                console.log(body);
+                // console.log(body);
                 body = body.substring(0,i).concat("\\'").concat(body.substring(i+1,body.length));
-                console.log(body);             
+                // console.log(body);             
                 i +=2 ;   
             }
         }
+        console.log(body);
         return body.substring(2,body.length-1);
     }
 }

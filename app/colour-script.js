2a0683c14545e5f66af08c205f8abe4340587ac2
Karla Martins Spuldaro <karlamartinsspuldaro@cmail.carleton.ca>
2018-03-27 19:18:37 -0400

    added coloured keyboard

--- app/colour-script.js
+++ app/colour-script.js
@@ -10,8 +10,19 @@ const Red = "red",
       Grey = "silver";
 
 // 0 stands for angle effect
-const colors = [Red, Orange, Yellow, Green, Blue, Purple, Brown, Pink, Black, Grey,
-    "red0", "orange0", "yellow0", "green0", "blue0", "purple0", "brown0", "pink0", "black0", "grey0"];
+//const colours = [
+//    Red, "red0", Grey, "green0", Green,
+//    "blue0",Blue, "grey0", Orange, "orange0",
+//    Yellow, "yellow0", Black, "purple0", Purple,
+//    "brown0", Brown, "black0", Pink, "pink0"];
+
+const colours = [
+    Red, Green, Grey, Orange, Blue,
+    "red0", "green0", "grey0", "orange0", "blue0",
+    Yellow, Purple, Black, Brown, Pink,
+    "yellow0", "purple0", "black0", "brown0", "pink0"
+
+   ];
 
 const EMAIL = "EMAIL",
       BANK = "BANK",
@@ -23,7 +34,8 @@ let userPasswords = {
     SHOPPING: []
 }
 
-let currentPassword = [];
+let currentPassword = [],
+    currentTry = [];
 
 //function runs once the page DOM is ready for JavaScript code to execute
 $(document).ready(function(){
@@ -72,7 +84,7 @@ function createPw(pwType){
 
     // generate new password
     for(let i = 0; i < 5; i++){
-        currentPassword[i] = colors[Math.floor(Math.random() * colors.length)];
+        currentPassword[i] = colours[Math.floor(Math.random() * colours.length)];
     }
 
     //TODO Need to send it to the server at some point
@@ -81,7 +93,7 @@ function createPw(pwType){
     displayPwSequence(currentPassword);
 
     $("#PIN").append("Please authenticate your new "+  pwType + " password below\n");
-    $("#PIN").append(" * make sure to follow the same order *");
+    $("#PIN").append(" * make sure to follow the given order *\n\n");
 
     displayKeyboard();
 
@@ -91,11 +103,62 @@ function createPw(pwType){
 
 
 function displayKeyboard(){
-    $("#keyboard").append("<p><5x4 colour keyboard here></p>");
+    $("#keyboard").css("border","2px solid black");
+
+    let rows = 4,
+        cols = 5,
+        tableRows = [];
+
+    // make a counter for the colours array
+    let c = 0;
+
+
+    // create table row objects
+    for(let x = 0; x < rows; x++){
+        tableRows[x] = $('<tr>');
+    }
+
+    for(let i = 0; i < rows; i++){
+
+        let row = tableRows[i];
+        $("#keyboard").append(row);
+
+        // for each row, add coloured squares
+        for(let j = 0; j < cols; j++){
+
+
+            //TODO repeated code, see displayPwSequence(). Make a function if possible.
+            let colour = colours[c];
+            let last = colour[colour.length-1];
+            let key = $("<div>")
+                        .addClass("square")
+                         key.attr('id', colour);
+
+            if (last !== '0'){
+               // it's a solid colour
+               key.css("background-color",colour);
+            }
+
+            //add a click listener to each key
+            key.click (function (chooseKey){
+                let self = $(this);
+                currentTry.push(this.getAttribute("id"));
+
+
+                //TODO remove this later
+                console.log("key pressed: " + this.getAttribute("id"));
+            });
+
+            row.append(key);
+            c++;
+        }
+    }
+
+
 }
 
 function registerPw(){
-    //TODO need to check for correctness, once we have a colour keyboard
+    //TODO need to check for correctness
 
 
     window.alert("Congratulations! Your new password is in our system.");
@@ -118,10 +181,11 @@ function registerPw(){
     });
 
     // clear divs
+    $("#sequence").text("");
     $("#PIN").text("");
-    $("#keyboard").text("");
+    $("#keyboard").text("")
+                  .css('border','');
 }
-
 
 function disableProperty(buttonId, disable){
     document.getElementById(buttonId).disabled = disable;
@@ -139,19 +203,17 @@ function displayPwSequence(sequence){
 
     // populate row with new elements
     for(let i = 0; i < pinSize; i++){
+
+        let colour = sequence[i];
+        let last = colour[colour.length-1];
+
         let square = $("<div>")
             .addClass("square")
-            .attr({row: 0, col: i});
+            .attr('id', colour);
 
-         let colour = sequence[i];
-         let last = colour[colour.length-1];
          if (last !== '0'){
             // it's a solid colour
             square.css("background-color",colour);
-         }
-         else{
-            // it's striped
-            square.attr('id', colour);
          }
 
         sequenceRow.append(square);
@@ -160,5 +222,5 @@ function displayPwSequence(sequence){
     $("#PIN").append("1                  2                  3                  4                  5\n");
 
     //TODO Delete this when everything else is working
-    $("#PIN").append("< "+ sequence + " >" + '\n\n');
+    console.log("PIN: < "+ sequence + " >" + '\n\n');
 }
